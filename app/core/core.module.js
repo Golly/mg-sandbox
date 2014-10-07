cd9a6a64bc71f8a0a5f5d6e70198bf75687c9852
MartinG <golan.martin@gmail.com>
2014-10-07 16:20:48 +0200

    layout with ui-router

--- app/core/core.module.js
+++ app/core/core.module.js
@@ -13,6 +13,6 @@
         /*
          * 3rd Party modules
          */
-        'ui.router'
+        'ui.router', 'wu.masonry'
     ]);
 })();
--- app/people/people.list.js
+++ app/people/people.list.js
@@ -12,6 +12,23 @@
         /*jshint validthis: true */
         var vm = this;
         vm.toto = 'test';
+        vm.bricks = [
+            {name: 'test', src: 'http://lorempixel.com/250/300/people/'},
+            {name: 'lide', src: 'http://lorempixel.com/250/400/people/'},
+            {name: 'lide3', src: 'http://lorempixel.com/250/250/people/'},
+            {name: 'lide4', src: 'http://lorempixel.com/250/160/people/'},
+            {name: 'lide5', src: 'http://lorempixel.com/250/500/people/'},
+            {name: 'test6', src: 'http://lorempixel.com/250/340/people/'},
+            {name: 'lide7', src: 'http://lorempixel.com/250/420/people/'},
+            {name: 'lide8', src: 'http://lorempixel.com/250/210/people/'},
+            {name: 'lide9', src: 'http://lorempixel.com/250/250/people/'},
+            {name: 'lide10', src: 'http://lorempixel.com/250/405/people/'},
+            {name: 'test11', src: 'http://lorempixel.com/250/325/sports/'},
+            {name: 'lide12', src: 'http://lorempixel.com/250/420/people/'},
+            {name: 'lide13', src: 'http://lorempixel.com/250/210/people/'},
+            {name: 'lide14', src: 'http://lorempixel.com/250/250/people/'},
+            {name: 'lide15', src: 'http://lorempixel.com/250/405/people/'}
+        ];
 
     }
 })();
